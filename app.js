a0a8c0cef8a414ba990bdded6a5cbeae3c912dbe
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-10-18 16:05:37 +0200

    Update code

--- app.js
+++ app.js
@@ -10,38 +10,37 @@ const express = require('express'),
     Auth0Strategy = require('passport-auth0'),
     flash = require('connect-flash'),
     routes = require('./routes/index'),
-    user = require('./routes/user');
+    user = require('./routes/user'),
+    app = express(),
 
-
-// This will configure Passport to use Auth0
-const strategy = new Auth0Strategy(
-  {
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-  },
-  (accessToken, refreshToken, extraParams, profile, done) => {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  }
-);
+    // This will configure Passport to use Auth0
+    strategy = new Auth0Strategy(
+        {
+            domain: process.env.AUTH0_DOMAIN,
+            clientID: process.env.AUTH0_CLIENT_ID,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET,
+            callbackURL:
+            process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+        },
+        (accessToken, refreshToken, extraParams, profile, done) => {
+            // accessToken is the token to call Auth0 API (not needed in the most cases)
+            // extraParams.id_token has the JSON Web Token
+            // profile has all the information from the user
+            return done(null, profile);
+        }
+    );
 
 passport.use(strategy);
 
 // you can use this section to keep a smaller payload
 passport.serializeUser((user, done) => {
-  done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-  done(null, user);
+    done(null, user);
 });
 
-const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -54,11 +53,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
-  session({
-    secret: 'shhhhhhhhh',
-    resave: true,
-    saveUninitialized: true
-  })
+    session({
+        secret: 'shhhhhhhhh',
+        resave: true,
+        saveUninitialized: true
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -68,22 +67,22 @@ app.use(flash());
 
 // Handle auth failure error messages
 app.use((req, res, next) => {
- if (req && req.query && req.query.error) {
-   req.flash("error", req.query.error);
- }
- if (req && req.query && req.query.error_description) {
-   req.flash("error_description", req.query.error_description);
- }
- next();
+    if (req && req.query && req.query.error) {
+        req.flash("error", req.query.error);
+    }
+    if (req && req.query && req.query.error_description) {
+        req.flash("error_description", req.query.error_description);
+    }
+    next();
 });
 
 // Check logged in
 app.use((req, res, next) => {
-  res.locals.loggedIn = false;
-  if (req.session.passport && typeof req.session.passport.user != 'undefined') {
-    res.locals.loggedIn = true;
-  }
-  next();
+    res.locals.loggedIn = false;
+    if (req.session.passport && typeof req.session.passport.user != 'undefined') {
+        res.locals.loggedIn = true;
+    }
+    next();
 });
 
 app.use('/', routes);
@@ -91,9 +90,9 @@ app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -101,23 +100,23 @@ app.use((req, res, next) => {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
