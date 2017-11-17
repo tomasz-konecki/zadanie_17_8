92eab0c05ff06a5ab8cdd99bd884e069103a5a65
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-11-17 16:42:03 +0100

    Add profile.js

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const express = require('express'),
 
 /* GET user profile. */
 router.get('/', ensureLoggedIn, (req, res, next) => {
+    console.log(req.user);
   res.render('user', {
     user: req.user
   });
